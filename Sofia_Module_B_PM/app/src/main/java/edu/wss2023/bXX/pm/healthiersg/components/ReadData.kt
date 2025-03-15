56feac10bef01e75f3b608f28982eefc75a3fd51
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-03-15 15:55:19 +0800

    Create Appointments Screen Layout

--- Sofia_Module_B_PM/app/src/main/java/edu/wss2023/bXX/pm/healthiersg/components/ReadData.kt
+++ Sofia_Module_B_PM/app/src/main/java/edu/wss2023/bXX/pm/healthiersg/components/ReadData.kt
@@ -1,10 +1,15 @@
 package edu.wss2023.bXX.pm.healthiersg.components
 
 import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.ui.platform.LocalContext
 import edu.wss2023.bXX.pm.healthiersg.R
 import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 data class Event(
     val id: String,
@@ -12,6 +17,31 @@ data class Event(
     val description: String,
     val dataTime: String,
     val location: String
+)
+
+data class Procedures(
+    val blood: Boolean = false,
+    val urine: Boolean = false,
+    val fast: Boolean = false
+)
+
+data class Appointments(
+    val id: String,
+    val date: String,
+    val time: String,
+    val doctor: String,
+    val department: String,
+    val type: String,
+//    val procedures: Procedures,
+    val clinic: String
+)
+
+data class Clinic(
+    val clinic: String,
+    val name: String,
+    val email: String,
+    val phone: String,
+    val address: String
 )
 
 fun readNorth(context: Context): List<Event>{
@@ -128,3 +158,68 @@ fun readCentral(context: Context): List<Event>{
     }
     return eventList
 }
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun readUpcoming(context: Context): List<Appointments>{
+    val inputStream = context.resources.openRawResource(R.raw.appointments)
+    val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+    val jsonString = bufferedReader.use { it.readText() }
+    val jsonObject = JSONObject(jsonString)
+    val jsonArray = jsonObject.getJSONArray("appointments")
+
+    val date = LocalDate.now()
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
+    var apps = mutableListOf<Appointments>()
+
+    for(i in 0 until jsonArray.length()){
+        val item = jsonArray.getJSONObject(i)
+        val givenDate = LocalDate.parse(item.getString("Date"), formatter)
+        if(givenDate.isBefore(date)){
+            val app = Appointments(
+                id = item.getString("Appointment_ID"),
+                date = item.getString("Date"),
+                time = item.getString("Time"),
+                doctor = item.getString("Doctor_Name"),
+                department = item.getString("Department"),
+                type = item.getString("Appointment_Type"),
+                clinic = item.getString("Clinic_ID")
+            )
+            apps.add(app)
+        }
+    }
+    return apps
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun readPast(context: Context): List<Appointments>{
+    val inputStream = context.resources.openRawResource(R.raw.appointments)
+    val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+    val jsonString = bufferedReader.use { it.readText() }
+    val jsonObject = JSONObject(jsonString)
+    val jsonArray = jsonObject.getJSONArray("appointments")
+
+    val date = LocalDate.now()
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
+    var apps = mutableListOf<Appointments>()
+
+    for(i in 0 until jsonArray.length()){
+        val item = jsonArray.getJSONObject(i)
+        val givenDate = LocalDate.parse(item.getString("Date"), formatter)
+        if(givenDate.isAfter(date)){
+            val app = Appointments(
+                id = item.getString("Appointment_ID"),
+                date = item.getString("Date"),
+                time = item.getString("Time"),
+                doctor = item.getString("Doctor_Name"),
+                department = item.getString("Department"),
+                type = item.getString("Appointment_Type"),
+                clinic = item.getString("Clinic_ID")
+            )
+            apps.add(app)
+        }
+    }
+    return apps
+}
+
--- Sofia_Module_B_PM/app/src/main/java/edu/wss2023/bXX/pm/healthiersg/screens/Appointments.kt
+++ Sofia_Module_B_PM/app/src/main/java/edu/wss2023/bXX/pm/healthiersg/screens/Appointments.kt
@@ -1,29 +1,118 @@
 package edu.wss2023.bXX.pm.healthiersg.screens
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import edu.wss2023.bXX.pm.healthiersg.components.EventCard
 import edu.wss2023.bXX.pm.healthiersg.components.NavigationDrawer
+import edu.wss2023.bXX.pm.healthiersg.components.readPast
+import edu.wss2023.bXX.pm.healthiersg.components.readUpcoming
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun Appointments(
     toEvents: () -> Unit,
     toAppointments: () -> Unit,
     toRecordings: () -> Unit,
-){
+) {
+    val upcoming = readUpcoming(LocalContext.current)
+    val past = readPast(LocalContext.current)
+
     NavigationDrawer(
         "Appointments", toEvents, toAppointments, toRecordings
     ) {
-        Column (
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
+        LazyColumn(
+            modifier = Modifier
+                .padding(top = 100.dp, start = 64.dp, end = 64.dp)
+                .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            Text("Appointments")
+        ) {
+            item {
+                Text(
+                    text = "UPCOMING APPOINTMENTS ",
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Start,
+                    letterSpacing = 5.sp,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+            item {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(300.dp)
+                ) {
+                    items(upcoming.size) { index ->
+                        var item = upcoming[index]
+                        Row (
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                        ){
+                            TextButton(onClick = {}) {
+                                Text(item.type)
+                            }
+                            Text(
+                                item.date + item.time,
+                                fontSize = 12.sp
+                            )
+                        }
+                    }
+                }
+            }
+            item {
+                Text(
+                    text = "PAST APPOINTMENTS",
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Start,
+                    letterSpacing = 5.sp,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+            item {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(300.dp)
+                ) {
+                    items(past.size) { index ->
+                        var item = past[index]
+                        Row (
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                        ){
+                            TextButton(onClick = {}) {
+                                Text(item.type)
+                            }
+                            Text(
+                                item.date + item.time,
+                                fontSize = 12.sp
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
